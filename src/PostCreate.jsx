ed3c0189762231d835bf9394e486774100756f66
iriyamakotoe <iriyamakotoe@icloud.com>
2024-07-16 01:05:11 +0900

    React05_スレッド内にメッセージ送信フォームを作ろう

--- src/PostCreate.jsx
+++ src/PostCreate.jsx
@@ -1,23 +1,15 @@
 import React, {useState, useCallback, useEffect} from 'react'
 
 export const PostCreate = (props) => {
-  const [createPost, setcreatePost] = useState('');
-  const newPost = () => {
+  // 親コンポーネントから受け取った関数を使って、inputの値を渡す
+  const handleClick = (event) => {
     const title = document.getElementById("post_title").value;
-    const requestOptions = {
-      method: 'POST',
-      headers:{'Content-Type': 'application/json'},
-      body: JSON.stringify({post: title})
-    };
-    fetch('https://railway.bulletinboard.techtrain.dev/threads/'+ props.urlParameters.id +'/posts',requestOptions)
-    .then(res => res.json())
-    .then(json => setcreatePost(json.post))
-  }
-  // const data = [{id: 'a56f455d-aa7e-4963-a56f-9fb94e15097f', post: 'test'}];
-  // console.log(data)
+    props.handleClick(title);
+  };
   return <>
-    <p><label><input type="text" id="post_title" /></label></p>
-    <p><button onClick={() => props.childToParent(newPost)}>作成</button></p>
+        <h2>投稿フォーム</h2>
+        <p><label><input type="text" id="post_title" /></label></p>
+        <p><button onClick={handleClick}>作成</button></p>
   </>
 }
 
--- src/PostsList.jsx
+++ src/PostsList.jsx
@@ -1,20 +1,16 @@
 import React, {useState, useCallback, useEffect} from 'react'
 import { useParams } from "react-router-dom";
 import Header from './Header';
-// import PostCreate from './PostCreate';
+import PostCreate from './PostCreate';
 
 export const PostsList = () => {
-  console.log('PostsList');
   const header = {text: 'トップに戻る', url: '/'};
   const urlParameters = useParams();
+
   const [posts, setPosts] = useState([]);
-  const [createPost, setcreatePost] = useState('');
-  // const childToParent = useCallback((data) => {
-  //   console.log('new post',data);
-  //   setcreatePost(data);
-  // },[])
-  const newPost = () => {
-    const title = document.getElementById("post_title").value;
+  const [newPost, setNewPost] = useState('');
+
+  const handleClick = (title) => {
     const requestOptions = {
       method: 'POST',
       headers:{'Content-Type': 'application/json'},
@@ -22,18 +18,21 @@ export const PostsList = () => {
     };
     fetch('https://railway.bulletinboard.techtrain.dev/threads/'+ urlParameters.id +'/posts',requestOptions)
     .then(res => res.json())
-    .then(json => setcreatePost(json.post))
+    .then(json => setNewPost(title))
   }
+
   useEffect(() => {
     fetch('https://railway.bulletinboard.techtrain.dev/threads/'+ urlParameters.id +'/posts?offset=0')
       .then(res => res.json())
       .then(json => setPosts(json.posts))
       console.log('fetch');
-  },[createPost])
+  },[newPost])
+
   const listItems = posts.map((obj) =>
     <li key={obj.id}>{obj.post=='' ? '（タイトルなし）'
       : obj.post}</li>
   );
+
   return <>
     <Header header={header} />
     <main>
@@ -43,13 +42,7 @@ export const PostsList = () => {
             ? <ul className='threadList'>{listItems}</ul>
             : <p>投稿はありません。</p>
           }
-
-        <h2>投稿フォーム</h2>
-        <p><label><input type="text" id="post_title" /></label></p>
-        <p><button onClick={newPost}>作成</button></p>
-
-
-        {/* <PostCreate urlParameters={urlParameters} childToParent={childToParent} /> */}
+        <PostCreate urlParameters={urlParameters} handleClick={handleClick} />
       </section>
     </main>
   </>
